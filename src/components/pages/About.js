b559f8e54bf9a119b12d6524e7621e38862ee214
jonjon <jonjonclark@gmail.com>
2019-08-16 08:41:56 +0200

    copy changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 
 const About = () => {
-    return (
-        <Fragment>
-            <h2> About Denham & About Jon Jon </h2>
-            <h3>  </h3>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <h2> About Denham & About Jon Jon </h2>
+      <h3>
+        Denham and JonJon met in their computer science honours class. Denham
+        looked like he had no friends and JonJon took a leap of faith and
+        invited him to join the cool kids for lunch. They have been building
+        cool things ever since.
+      </h3>
+      <br />
+      <h3>
+        Togther they are currently working on wildcards.world and
+        alwaysforsale.io - unique projects that combine interesting incetive
+        mechanisms. The success of these projects has led them to a move to
+        Station F in Paris where they will continue to build.
+      </h3>
+      <br />
+    </Fragment>
+  );
+};
 
-export default About
+export default About;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,27 +1,43 @@
 import React, { Fragment } from 'react';
-import applePodcastsImg from '../../img/applepodcasts.png'
-import spotifyPodcastsImg from '../../img/spotifypodcasts.png'
-import googlePodcastsImg from '../../img/googlepodcasts.png'
+import applePodcastsImg from '../../img/applepodcasts.png';
+import spotifyPodcastsImg from '../../img/spotifypodcasts.png';
+import googlePodcastsImg from '../../img/googlepodcasts.png';
 
 const Home = () => {
   return (
     <div>
-      <div className="grid-2">
+      <div className='grid-2'>
         <div>
           <h1>Coming soon...</h1>
-          <p>Denham and JonJon Talk Startups is a 45min podcast on disruptive
-             startups in South Africa with a strong focus on tech</p>
-             <br/>
-             <ul>
-               <li>#1 - <strong>Invictus Capital</strong> with Co-founder Matt Finlay</li>
-               <li>#2 - </li>
-               <li></li>
-             </ul>
+          <p>
+            Interesting in learning from the top minds that formulated some of
+            the most innovative startups?
+            <br />
+            You've come to the right place. Denham and JonJon talk to founders
+            and leaders of disruptive start ups that focus on tech.
+            <br />
+            Season 1 focusses on start ups based in Cape Town, South Africa.
+          </p>
+          <br />
+          <ul>
+            <li>
+              #1 - <strong>Invictus Capital</strong> with Co-founder Matt
+              Finlayson
+            </li>
+            <li>
+              #2 - <strong>House Me</strong> with Co-founder and CTO Kyle
+              Bradley
+            </li>
+            <li>
+              #3 - <strong>Molecule.io</strong> with lead dev Kent{' '}
+            </li>
+            <li />
+          </ul>
         </div>
         <div>
           <img src={applePodcastsImg} />
-          <img src={spotifyPodcastsImg}/>
-          <img src={googlePodcastsImg}/>
+          <img src={spotifyPodcastsImg} />
+          <img src={googlePodcastsImg} />
         </div>
       </div>
     </div>
