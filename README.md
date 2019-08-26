707916c916d5ebe46c005b89c2933dcf9a22f8d5
denhampreen <denhampreen@gmail.com>
2019-08-26 13:22:22 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# DenhamAndJonJonTalkStartups
-
-> React Static Marketing Site for Denham and Jon Jon Talk Start Ups
--- src/components/pages/Episodes.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-
-const Episodes = () => (
-  <div className='container'>
-    <h1>Coming Soon</h1>
-  </div>
-);
-
-export default Episodes;
